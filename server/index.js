119500cd25c82db338a599d5b04140ac6999c4bb
KimJeongHyun <ptr1129@hanmail.net>
2021-08-27 19:28:15 +0900

    first commit

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const path = require('path');
+
+
+const port = 5000;
+
+app.use(express.json({
+    limit:'50mb'
+})); 
+app.use(express.urlencoded({
+    limit:'50mb',extended : true
+})) 
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+
+const server = http.createServer(app);
+server.listen(port, ()=>console.log(`Server Start. Port : ${port}`))
+
+
