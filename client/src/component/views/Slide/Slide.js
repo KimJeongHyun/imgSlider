b67939f6d53e06ca0da3ba898cb1fb673e0757b5
KimJeongHyun <ptr1129@hanmail.net>
2021-08-31 22:38:28 +0900

    move prev/next image clear.

--- client/src/component/views/Slide/Slide.js
+++ client/src/component/views/Slide/Slide.js
@@ -9,7 +9,8 @@ class Slide extends Component{
         super(props);
         this.state={
             FileMap:'',
-            startKey:'',
+            startKey:0,
+            nowLoc:0,
             length:''
         }
     }
@@ -23,7 +24,8 @@ class Slide extends Component{
             for (let key in this.state.FileMap){
                 if (cnt==0){
                     this.setState({
-                        startKey:key
+                        startKey:key,
+                        nowLoc:key
                     })
                 }
                 cnt++;
@@ -36,14 +38,46 @@ class Slide extends Component{
     
     componentDidUpdate(prevProps){
         const result = [];
-        if (this.state.length!=''){
+        if (this.state.length!='' && this.state.nowLoc==this.state.startKey){
             const str = this.state.FileMap[this.state.startKey].substring(14);
             result.push(
                 <img src={str} style={{width:'90%'}}/>
             )
             ReactDOM.render(result,document.getElementById('imgContainer'))
-        }      
+        }
+        if (this.state.length!='' && this.state.nowLoc!=this.state.startKey){
+            const str = this.state.FileMap[this.state.nowLoc].substring(14);
+            result.push(
+                <img src={str} style={{width:'90%'}}/>
+            )
+            ReactDOM.render(result,document.getElementById('imgContainer'))
+        }
     }
+
+    prevOnClick = () =>{
+        if (this.state.nowLoc==this.state.startKey){
+            alert('첫번째 이미지입니다.');
+        }else{
+            const locVar = parseInt(this.state.nowLoc);
+            this.setState({
+                nowLoc:locVar-1
+            })
+            
+        }
+    }
+
+    nextOnClick = () =>{
+        if (this.state.nowLoc == this.state.length-1){
+            alert('마지막 이미지입니다.');
+        }else{
+            const locVar = parseInt(this.state.nowLoc);
+            this.setState({
+                nowLoc:locVar+1
+            })
+        }
+    }
+
+
 
     render(){
         return(
@@ -51,6 +85,8 @@ class Slide extends Component{
                 <NavBar/>
                 <div style={{display:'flex', flexDirection:'column',justifyContent:'center',alignItems: 'center',
             width:'100%',height:'100vh', marginTop:'10vh', position:'relative'}}>
+                <button onClick={this.prevOnClick}>prev</button>
+                <button onClick={this.nextOnClick}>next</button>
                 <div id="imgContainer" style={{border:'0.2rem solid', width:'40%', height:'70%',textAlign:'center', display:'flex',justifyContent:'center',alignItems:'center'}}>
 
                 </div>   
